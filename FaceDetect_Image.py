aebd3b87f15e160c1c6cb6b588ebb3f69bb6fb11
ZealousGuy <112643891+ZealousGuy@users.noreply.github.com>
2022-11-29 19:28:18 +0530

    Add files via upload

---
+++ FaceDetect_Image.py
@@ -0,0 +1,18 @@
+# importing OpenCV
+import cv2 as c
+# loading haar data for face detection
+haar_data=c.CascadeClassifier("Face_detection/haarcascade_frontalface_default.xml")
+
+# -->Reading Image for Input
+img = c.imread("photos/photo-1501386761578-eac5c94b800a.jpeg")
+
+# Detecting the face 
+faces=haar_data.detectMultiScale(img)
+
+# Drawing recctangle around Faces in image
+for x,y,w,h in faces:
+    c.rectangle(img,(x,y),(x+w,y+h),(255,5,5),2)
+
+# Displaying the image in another window
+c.imshow('res',img)
+c.waitKey() 
